ea33ff234b3bf82561d7f31e8546e2d5445db601
tianhz <tianhzch@gmail.com>
2021-08-29 01:22:06 +0800

    add default code template

---
+++ default.cpp
@@ -0,0 +1,23 @@
+/*****************************************************
+ * Problem: 
+ * Author: _thz
+ * Date: 
+ ****************************************************/
+#include <cstdio>
+
+#define DEBUG
+
+int main()
+{
+  #ifdef DEBUG
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+  #endif // DEBUG
+  
+  #ifdef DEBUG
+    fclose(stdin);
+    fclose(stdout);
+  #endif // DEBUG
+
+  return 0;
+}
