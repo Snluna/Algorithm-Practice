c26db9195df28c48dd4ddc6de614f2219728ce9f
tianhz <tianhzch@gmail.com>
2021-09-04 00:42:23 +0800

    update

--- default.cpp
+++ default.cpp
@@ -5,18 +5,8 @@
  ****************************************************/
 #include <cstdio>
 
-#define DEBUG
-
 int main() {
-#ifdef DEBUG
-  freopen("in.txt", "r", stdin);
-  freopen("out.txt", "w", stdout);
-#endif  // DEBUG
-
-#ifdef DEBUG
-  fclose(stdin);
-  fclose(stdout);
-#endif  // DEBUG
+  printf("hello world!\n");
 
   return 0;
 }
